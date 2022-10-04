565c3c919ffa01273fea16f6a4935f0d79975609
Siounath <82611145+Esiounath@users.noreply.github.com>
2022-10-04 19:35:05 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import {Clock} from './Clock';
+//import {Form} from './form';
+
+function App() {
+  return (
+<body>
+    <div className="App">
+      <header className="App-header">
+      <Clock/>
+      </header>
+    </div>
+</body>
+  );
+}
+
+export default App;
